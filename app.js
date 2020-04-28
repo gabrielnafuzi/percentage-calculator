ed0918e8e9910a253b646727ae513f29dff76dbb
nafuzi <gabrielnafuzi@gmail.com>
2020-04-28 20:23:07 -0300

    few changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ firstButton.addEventListener("click", () => {
   const firstResult = document.querySelector(".firstResult");
   const a = document.querySelector(".a").value;
   const b = document.querySelector(".b").value;
-  result = ((a * b) / 100).toFixed(4);
+  result = (a * b) / 100;
   firstResult.value = result;
 });
 
@@ -15,14 +15,14 @@ secondButton.addEventListener("click", () => {
   const secondResult = document.querySelector(".secondResult");
   const c = document.querySelector(".c").value;
   const d = document.querySelector(".d").value;
-  result = ((c * 100) / d).toFixed(4);
-  secondResult.value = result + "%";
+  result = (c * 100) / d;
+  secondResult.value = `${result}%`;
 });
 
 thirdButton.addEventListener("click", () => {
   const thirdResult = document.querySelector(".thirdResult");
   const e = document.querySelector(".e").value;
   const f = document.querySelector(".f").value;
-  result = ((e / f) * 100).toFixed(4);
+  result = (e / f) * 100;
   thirdResult.value = result;
 });
