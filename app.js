741ff878a2d485e7a46957f2e656033643365086
nafuzi <gabrielnafuzi@gmail.com>
2020-04-28 18:41:56 -0300

    mudanças no js

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const firstButton = document.querySelector('.firstButton');
-const secondButton = document.querySelector('.secondButton');
-const thirdButton = document.querySelector('.thirdButton');
+const firstButton = document.querySelector(".firstButton");
+const secondButton = document.querySelector(".secondButton");
+const thirdButton = document.querySelector(".thirdButton");
+let result;
 
-firstButton.addEventListener('click', firstAccount);
-secondButton.addEventListener('click', secondAccount);
-thirdButton.addEventListener('click', thirdAccount);
-
-function firstAccount() {
-  const firstResult = document.querySelector('.firstResult');
-  const a = document.querySelector('.a').value;
-  const b = document.querySelector('.b').value;
-  const result = (a * b) / 100;
+firstButton.addEventListener("click", () => {
+  const firstResult = document.querySelector(".firstResult");
+  const a = document.querySelector(".a").value;
+  const b = document.querySelector(".b").value;
+  result = ((a * b) / 100).toFixed(4);
   firstResult.value = result;
-}
+});
 
-function secondAccount() {
-  const secondResult = document.querySelector('.secondResult');
-  const c = document.querySelector('.c').value;
-  const d = document.querySelector('.d').value;
-  const result = (c * 100) / d;
+secondButton.addEventListener("click", () => {
+  const secondResult = document.querySelector(".secondResult");
+  const c = document.querySelector(".c").value;
+  const d = document.querySelector(".d").value;
+  result = ((c * 100) / d).toFixed(4);
   secondResult.value = result + "%";
-}
+});
 
-function thirdAccount() {
-  const thirdResult = document.querySelector('.thirdResult');
-  const e = document.querySelector('.e').value;
-  const f = document.querySelector('.f').value;
-  const result = (e / f) * 100;
+thirdButton.addEventListener("click", () => {
+  const thirdResult = document.querySelector(".thirdResult");
+  const e = document.querySelector(".e").value;
+  const f = document.querySelector(".f").value;
+  result = ((e / f) * 100).toFixed(4);
   thirdResult.value = result;
-}
+});
