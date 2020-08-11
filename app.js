21d42a54df699185003cd6a2135d7f39b9309caf
gabrielnafuzi <gabrielnafuzi@gmail.com>
2020-08-11 02:27:26 -0300

    format resp input value

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
-const firstButton = document.querySelector(".firstButton");
-const secondButton = document.querySelector(".secondButton");
-const thirdButton = document.querySelector(".thirdButton");
-let result;
+const firstButton = document.querySelector('.firstButton');
+const secondButton = document.querySelector('.secondButton');
+const thirdButton = document.querySelector('.thirdButton');
+const firstResult = document.querySelector('.firstResult');
+const secondResult = document.querySelector('.secondResult');
+const thirdResult = document.querySelector('.thirdResult');
 
-firstButton.addEventListener("click", () => {
-  const firstResult = document.querySelector(".firstResult");
-  const a = document.querySelector(".a").value;
-  const b = document.querySelector(".b").value;
+let result = 0;
+
+function formatNumber(number) {
+  if (!Number.isInteger(number)) {
+    return number.toFixed(2);
+  }
+}
+
+firstButton.addEventListener('click', () => {
+  const a = document.querySelector('.a').value;
+  const b = document.querySelector('.b').value;
   result = (a * b) / 100;
+
+  result = formatNumber(result);
+
   firstResult.value = result;
 });
 
-secondButton.addEventListener("click", () => {
-  const secondResult = document.querySelector(".secondResult");
-  const c = document.querySelector(".c").value;
-  const d = document.querySelector(".d").value;
+secondButton.addEventListener('click', () => {
+  const c = document.querySelector('.c').value;
+  const d = document.querySelector('.d').value;
   result = (c * 100) / d;
+
+  result = formatNumber(result);
+
   secondResult.value = `${result}%`;
 });
 
-thirdButton.addEventListener("click", () => {
-  const thirdResult = document.querySelector(".thirdResult");
-  const e = document.querySelector(".e").value;
-  const f = document.querySelector(".f").value;
+thirdButton.addEventListener('click', () => {
+  const e = document.querySelector('.e').value;
+  const f = document.querySelector('.f').value;
   result = (e / f) * 100;
+
+  result = formatNumber(result);
+
   thirdResult.value = result;
 });
